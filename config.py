ad6f70bfc48e7684d845fae91fd6d50c3cdb26df
Константин <sazanovicila@gmail.com>
2025-09-01 21:25:07 +0300

    Добавление webhook secret в config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
     USE_NEW_ORDER_FLOW = False
 
 config = Config()
